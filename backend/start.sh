f42b3f5caa2f320e268b87a96dc7061f9ae3413c
Jefferson Silva <jefferson_jsp@hotmail.com>
2026-03-10 14:27:16 -0300

    add nginx.conf

---
+++ backend/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+echo "Waiting for postgres..."
+while ! nc -z postgres 5432; do
+  sleep 1
+done
+echo "PostgreSQL started"
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+gunicorn core.wsgi:application --bind 0.0.0.0:9999 --workers 3
