d52b8bce6d235f1c7352bde44e0c5ef7d2ed2be7
Jefferson Silva <jefferson_jsp@hotmail.com>
2026-03-10 14:53:56 -0300

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for postgres..."
-while ! nc -z gerenciai-postgres 5432; do
-  sleep 1
-done
-echo "PostgreSQL started"
-
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
